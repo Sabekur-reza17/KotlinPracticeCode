51d57c2797a967f25ebc84277bf16c1efa2d7da6
sabekur <sabekur2020@gmail.com>
2020-01-07 11:18:58 +0600

    Kotlin practice started

---
+++ src/basicKotlin/HelloKotlin.kt
@@ -0,0 +1,5 @@
+package basicKotlin
+
+fun main(args: Array<String>) {
+    println("Hello kotlin")
+}
---
+++ src/basicKotlin/userInputEx1.kt
@@ -0,0 +1,8 @@
+package basicKotlin
+
+fun main(args: Array<String>) {
+    println("Enter your text ")
+    val userInput= readLine()!!
+    println("your entered text : $userInput")
+
+}
